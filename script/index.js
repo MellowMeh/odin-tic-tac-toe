9d234dc9721af4c240af5993eb69a18622ceec5a
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-15 20:25:47 -0400

    Add easyAI array modifier function
    Player will be able to choose easy or hard AI to play
    against. Easy AI selects a completely random index to
    modify. Negative one is used to indicate the computer has
    selected that cell.

--- script/index.js
+++ script/index.js
@@ -17,9 +17,57 @@ let PlayerFactory = function (name, status) {
     return player;
 };
 
+const easyAI = (function () {
+    const randomDecimal = Math.random();
+    const getRandomNumber = (randomDecimal*100);
+    console.log(getRandomNumber);
+    const changeIndex = () => {
+        if (getRandomNumber < 12) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[0][0] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 23) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[0][1] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 34) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[0][2] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 45) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[1][0] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 56) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[1][1] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 67) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[1][2] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 78) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[2][0] = -1;
+                console.table(createBoard.getBoard());
+        } else if (getRandomNumber < 89) {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[2][1] = -1;
+                console.table(createBoard.getBoard());
+        } else {
+            let gameBoard = createBoard.getBoard();
+                gameBoard[2][2] = -1;
+                console.table(createBoard.getBoard());
+        }
+    };
+    return {
+        getEasyAIIndex: () => changeIndex(),
+        
+    } 
+})();
+
 const playGame = (function () {
-    createBoard.getBoard();
-    console.table(createBoard.getBoard());
     let player1 = PlayerFactory('Robert', 'Human');
     console.log(player1);
+    easyAI.getEasyAIIndex();
 })();
